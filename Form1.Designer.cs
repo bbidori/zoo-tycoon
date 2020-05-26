eb7145e3c498158af1cb5bea2a6adc1988dd53cc
bbidori <bbidori@users.noreply.github.com>
2020-05-25 20:45:37 -0400

    creation zoo et clotures
    les clotures serviront en tant que delimiteur des sprites (des animaux)
    
    arriere-plant est le dessin du zoo

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cloture1 = new System.Windows.Forms.PictureBox();
+            this.cloture2 = new System.Windows.Forms.PictureBox();
+            this.cloture3 = new System.Windows.Forms.PictureBox();
+            this.cloture4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.cloture1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cloture2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cloture3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cloture4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cloture1
+            // 
+            this.cloture1.BackColor = System.Drawing.Color.Transparent;
+            this.cloture1.BackgroundImage = global::zoo_tycoon.Properties.Resources.dev;
+            this.cloture1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.cloture1.Location = new System.Drawing.Point(139, 120);
+            this.cloture1.Name = "cloture1";
+            this.cloture1.Size = new System.Drawing.Size(443, 281);
+            this.cloture1.TabIndex = 0;
+            this.cloture1.TabStop = false;
+            // 
+            // cloture2
+            // 
+            this.cloture2.BackColor = System.Drawing.Color.Transparent;
+            this.cloture2.BackgroundImage = global::zoo_tycoon.Properties.Resources.dev;
+            this.cloture2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.cloture2.Location = new System.Drawing.Point(646, 120);
+            this.cloture2.Name = "cloture2";
+            this.cloture2.Size = new System.Drawing.Size(443, 281);
+            this.cloture2.TabIndex = 1;
+            this.cloture2.TabStop = false;
+            // 
+            // cloture3
+            // 
+            this.cloture3.BackColor = System.Drawing.Color.Transparent;
+            this.cloture3.BackgroundImage = global::zoo_tycoon.Properties.Resources.dev;
+            this.cloture3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.cloture3.Location = new System.Drawing.Point(139, 467);
+            this.cloture3.Name = "cloture3";
+            this.cloture3.Size = new System.Drawing.Size(443, 281);
+            this.cloture3.TabIndex = 2;
+            this.cloture3.TabStop = false;
+            // 
+            // cloture4
+            // 
+            this.cloture4.BackColor = System.Drawing.Color.Transparent;
+            this.cloture4.BackgroundImage = global::zoo_tycoon.Properties.Resources.dev;
+            this.cloture4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.cloture4.Location = new System.Drawing.Point(646, 467);
+            this.cloture4.Name = "cloture4";
+            this.cloture4.Size = new System.Drawing.Size(443, 281);
+            this.cloture4.TabIndex = 3;
+            this.cloture4.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = global::zoo_tycoon.Properties.Resources.background_zoo2;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1597, 881);
+            this.Controls.Add(this.cloture4);
+            this.Controls.Add(this.cloture3);
+            this.Controls.Add(this.cloture2);
+            this.Controls.Add(this.cloture1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.cloture1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cloture2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cloture3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cloture4)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox cloture1;
+        private System.Windows.Forms.PictureBox cloture2;
+        private System.Windows.Forms.PictureBox cloture3;
+        private System.Windows.Forms.PictureBox cloture4;
     }
 }
 
